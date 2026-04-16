2e3c59809424ddac8428eaa225a1d40041c40ce4
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2026-04-15 22:40:37 -0300

    finish endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,35 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1+1');
-  console.log(result);
+  const updateAt = new Date().toISOString()
+
+  //database version
+  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
   
-  response.status(200).json({ chave: "valor" });
+  //database max connections
+  const databaseMaxConnectionsResults = await database.query("SHOW max_connections;")
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResults.rows[0].max_connections 
+
+  //database opened connections
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionsResults = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName]
+  }
+  )
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResults.rows[0].count;
+  
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: parseInt(databaseOpenedConnectionsValue)
+      }
+    }
+  });
 }
 
 export default status;
