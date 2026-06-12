1a87885482342705ae9e23ab4502dde5ca8e02a5
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2026-06-12 19:56:02 -0300

    fix: correct response handling in waitForWebServer function

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -6,12 +6,15 @@ async function waitForAllServices(params) {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000
     })
   }
 
   async function fetchStatusPage() {
-    const responde = await fetch("http://localhost:3000/api/v1/status")
-    const responseBody = await responde.json()
+    const response = await fetch("http://localhost:3000/api/v1/status")
+    if (response.status !== 200) {
+      throw Error();
+    }
   }
 }
 
