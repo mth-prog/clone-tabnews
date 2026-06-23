2afcae4c666ec23cc0f6e6bfc0f5e56ddb30af69
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2026-06-23 20:31:15 -0300

    fix: update expected database version in status test

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ test("GET to api/v1/status should return 200", async () => {
         const parsedUpdateAt = new Date(responseBody.updated_at).toISOString()
         expect(responseBody.updated_at).toEqual(parsedUpdateAt)
 
-        expect(responseBody.dependencies.database.version).toEqual("16.13")
+        expect(responseBody.dependencies.database.version).toEqual("16")
         expect(responseBody.dependencies.database.max_connections).toEqual(100)
         expect(responseBody.dependencies.database.open_connections).toEqual(1)
         
