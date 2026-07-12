ee10637503c1afa34a8a8d3f2d47cd8c2d4ebf50
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2026-07-12 12:04:40 -0300

    add lint: eslint check with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
+import prettier from "eslint-plugin-prettier";
+
+export default defineConfig([
+  { ignores: [".next/**", "node_modules/**", "dist/**"] },
+  { files: ["**/*.js"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.js"],
+    plugins: { prettier },
+    rules: { "prettier/prettier": "warn" },
+  },
+  {
+    files: ["**/*.test.js"],
+    languageOptions: { globals: { ...globals.browser, ...globals.jest } },
+    plugins: { jest },
+    rules: { ...jest.configs.recommended.rules },
+  },
+]);
