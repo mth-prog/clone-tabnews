d02fb25fd81337ab0ad56b326b304d583bd729a7
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2026-07-12 12:48:24 -0300

    fix: clean up eslint.config.mjs and remove unnecessary whitespace in wait-for-postgres.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,14 +8,23 @@ export default defineConfig([
   { ignores: [".next/**", "node_modules/**", "dist/**", "jest.config.js"] },
   { files: ["**/*.js"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
-  {files: ["**/*.js"], plugins: { prettier },rules: { "prettier/prettier": "warn" },},
-  {files: ["**/*.test.js"], languageOptions: { globals: { ...globals.browser, ...globals.jest } }, plugins: { jest }, rules: { ...jest.configs.recommended.rules },},
   {
-  files: ["infra/scripts/**/*.js", "jest.config.js"],
-  languageOptions: {
-    globals: globals.node,
-    ecmaVersion: "latest",
-    sourceType: "module"
-    }
+    files: ["**/*.js"],
+    plugins: { prettier },
+    rules: { "prettier/prettier": "warn" },
+  },
+  {
+    files: ["**/*.test.js"],
+    languageOptions: { globals: { ...globals.browser, ...globals.jest } },
+    plugins: { jest },
+    rules: { ...jest.configs.recommended.rules },
+  },
+  {
+    files: ["infra/scripts/**/*.js", "jest.config.js"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
   },
 ]);
