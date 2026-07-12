715f176280b135c0d12c4ab8f471f2227a5a8a8a
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2026-07-12 12:46:45 -0300

    fix: correct import statements in wait-for-postgres.js

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,5 +1,5 @@
-const { exec } = "node:child_process";
-import process from "node:process";
+const { exec } = require("node:child_process");
+
 
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
